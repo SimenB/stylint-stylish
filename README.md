c12f0389064542df029ed0b84b942859a4f3a7af
Simen Bekkhus <sbekkhus91@gmail.com>
2015-07-18 16:42:20 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # stylint-stylish
-Stylish reporter for Stylint
+> Stylish formatter for Stylint
+
+[![NPM version][npm-image]][npm-url] [![Build status][travis-image]][travis-url] [![Dependency status][david-image]][david-url]
+
+## Screenshots
+![This reporter][new-image]
+vs original:
+![Original reporter][orig-image]
+
+## Usage
+
+To use it, just set `reporter` to `stylint-stylish` in the config (`.stylintrc`) or when calling stylint programatically.
+
+```json
+{
+  "reporter": "stylint-stylish",
+  "noImportant": true,
+  "semicolons": {
+    "expect": "always",
+    "error": true
+  }
+}
+```
+
+
+[travis-url]: https://travis-ci.org/SimenB/stylint-stylish
+[travis-image]: https://img.shields.io/travis/SimenB/stylint-stylish.svg
+[npm-url]: https://npmjs.org/package/stylint-stylish
+[npm-image]: https://img.shields.io/npm/v/stylint-stylish.svg
+[david-url]: https://david-dm.org/SimenB/stylint-stylish
+[david-image]: https://img.shields.io/david/SimenB/stylint-stylish.svg
+[new-image]: screenshots/this.png
+[orig-image]: screenshots/original.png
---
+++ src/index.js
@@ -0,0 +1,73 @@
+import chalk from 'chalk';
+import logSymbols from 'log-symbols';
+import table from 'text-table';
+import pluralize from 'pluralize';
+import isNumber from 'lodash.isnumber';
+
+let currFile, currTable = [];
+
+function createSummary (errs, warns, total) {
+  if (total === 0) {
+    return 'No violations';
+  }
+
+  let output = '';
+
+  if (errs > 0) {
+    output += `\t${logSymbols.error}  ${pluralize('error', errs, true)}`;
+
+    if (isNumber(this.config.maxErrors)) {
+      output += ` (Max Errors: ${this.config.maxErrors})`;
+    }
+
+    output += '\n';
+  }
+
+  if (warns > 0) {
+    output += `\t${logSymbols.warning}  ${pluralize('warning', warns, true)}`;
+
+    if (isNumber(this.config.maxWarnings)) {
+      output += ` (Max Warnings: ${this.config.maxWarnings})`;
+    }
+
+    output += '\n';
+  }
+
+  return output;
+}
+
+function doneHandler (kill) {
+  const errs = this.cache.errs.length, warns = this.cache.warnings.length, total = errs + warns;
+
+  this.cache.msg = `${table(currTable)}\n${createSummary.call(this, errs, warns, total)}`;
+
+  if (kill === 'kill') {
+    this.cache.msg += '\nStylint: Over Error or Warning Limit.';
+  } else if (total === 0) {
+    this.cache.msg = `Stylint: You're all clear!`;
+  }
+
+  return this.done();
+}
+
+export default function (msg, done, kill) {
+  if (done === 'done') {
+    return doneHandler.call(this, kill);
+  }
+
+  const isWarning = this.state.severity === 'Warning';
+
+  if (currFile !== this.cache.file) {
+    currFile = this.cache.file;
+
+    currTable.push([ `${chalk.underline(currFile)}:\n` ]);
+  }
+
+  currTable.push([
+    '',
+    chalk.gray(`line ${this.cache.lineNo}:`),
+    isWarning ? (process.platform === 'win32' ? chalk.cyan(msg) : chalk.blue(msg)) : chalk.red(msg)
+  ]);
+
+  return '';
+}
