ea1d777578eb3e14fd9e54b55cc1508a1b7132c4
Simen Bekkhus <sbekkhus91@gmail.com>
2016-03-25 01:33:20 +0000

    use ava

--- test.js
+++ test.js
@@ -1,235 +1,239 @@
-/* eslint-env mocha */
+/* eslint-disable eqeqeq */
 
-import assert from 'assert'
 import chalk from 'chalk'
 import logSymbols from 'log-symbols'
+
 import stylint from 'stylint'
 import path from 'path'
 import clone from 'lodash.clonedeep'
 import origCache from 'stylint/src/core/cache'
 import origState from 'stylint/src/core/state'
 
-const stylintInstance = stylint().create()
+import test from 'ava'
+
+import 'babel-register'
 
 const errorIcon = chalk.stripColor(logSymbols.error)
 const warningIcon = chalk.stripColor(logSymbols.warning)
 
-describe('stylint-stylish', () => {
-  beforeEach(() => {
-    stylintInstance.state = clone(origState)
-    stylintInstance.cache = clone(origCache)
+test.beforeEach('before', t => {
+  const stylintInstance = stylint().create()
 
-    stylintInstance.state.quiet = true
-    stylintInstance.state.watching = true
-    stylintInstance.state.strictMode = false
-    stylintInstance.config.reporter = require.resolve('./stylish')
-    stylintInstance.config.reporterOptions = undefined
-    stylintInstance.config.maxErrors = undefined
-    stylintInstance.config.maxWarnings = undefined
+  stylintInstance.state = clone(origState)
+  stylintInstance.cache = clone(origCache)
 
-    stylintInstance.init()
-  })
+  stylintInstance.state.quiet = true
+  stylintInstance.state.watching = true
+  stylintInstance.state.strictMode = false
+  stylintInstance.config.reporter = require.resolve('./stylish')
+  stylintInstance.config.reporterOptions = undefined
+  stylintInstance.config.maxErrors = undefined
+  stylintInstance.config.maxWarnings = undefined
 
-  it('should report if all green', () => {
-    var report = stylintInstance.reporter('meh', 'done').msg
+  stylintInstance.init()
 
-    assert.equal('', report)
-  })
+  t.context.stylintInstance = stylintInstance
+})
 
-  it('should report violations', () => {
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.errs = ['', '']
-    stylintInstance.cache.warnings = ['']
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
-    stylintInstance.cache.lineNo = 10
-    stylintInstance.state.severity = 'Warning'
-    stylintInstance.reporter('dee')
-    stylintInstance.cache.file = 'meep.styl'
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('doo')
+test('should report if all green', t => {
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should report violations', t => {
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.errs = ['', '']
+  t.context.stylintInstance.cache.warnings = ['']
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
+  t.context.stylintInstance.cache.lineNo = 10
+  t.context.stylintInstance.state.severity = 'Warning'
+  t.context.stylintInstance.reporter('dee')
+  t.context.stylintInstance.cache.file = 'meep.styl'
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('doo')
 
-    assert.equal(report.length, 11)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3].trim(), 'line 10:  dee')
-    assert.equal(report[4], '')
-    assert.equal(report[5], 'meep.styl')
-    assert.equal(report[6].trim(), 'line 15:  doo')
-    assert.equal(report[7], '')
-    assert.equal(report[8].trim(), `${errorIcon}  2 errors`)
-    assert.equal(report[9].trim(), `${warningIcon}  1 warning`)
-    assert.equal(report[10], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should log Max Errors if provided', () => {
-    stylintInstance.config.maxErrors = 1
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.errs = ['', '']
-    stylintInstance.cache.warnings = []
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
-    stylintInstance.cache.lineNo = 10
-    stylintInstance.state.severity = 'Warning'
-    stylintInstance.reporter('dee')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 11)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3].trim() == 'line 10:  dee')
+  t.ok(report[4] == '')
+  t.ok(report[5] == 'meep.styl')
+  t.ok(report[6].trim() == 'line 15:  doo')
+  t.ok(report[7] == '')
+  t.ok(report[8].trim() == `${errorIcon}  2 errors`)
+  t.ok(report[9].trim() == `${warningIcon}  1 warning`)
+  t.ok(report[10] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should log Max Errors if provided', t => {
+  t.context.stylintInstance.config.maxErrors = 1
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.errs = ['', '']
+  t.context.stylintInstance.cache.warnings = []
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
+  t.context.stylintInstance.cache.lineNo = 10
+  t.context.stylintInstance.state.severity = 'Warning'
+  t.context.stylintInstance.reporter('dee')
 
-    assert.equal(report.length, 7)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3].trim(), 'line 10:  dee')
-    assert.equal(report[4], '')
-    assert.equal(report[5].trim(), `${errorIcon}  2 errors (Max Errors: 1)`)
-    assert.equal(report[6], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should log Max Warnings if provided', () => {
-    stylintInstance.config.maxWarnings = 1
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.warnings = ['', '']
-    stylintInstance.cache.errs = []
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
-    stylintInstance.cache.lineNo = 10
-    stylintInstance.state.severity = 'Warning'
-    stylintInstance.reporter('dee')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 7)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3].trim() == 'line 10:  dee')
+  t.ok(report[4] == '')
+  t.ok(report[5].trim() == `${errorIcon}  2 errors (Max Errors: 1)`)
+  t.ok(report[6] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should log Max Warnings if provided', t => {
+  t.context.stylintInstance.config.maxWarnings = 1
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.warnings = ['', '']
+  t.context.stylintInstance.cache.errs = []
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
+  t.context.stylintInstance.cache.lineNo = 10
+  t.context.stylintInstance.state.severity = 'Warning'
+  t.context.stylintInstance.reporter('dee')
 
-    assert.equal(report.length, 7)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3].trim(), 'line 10:  dee')
-    assert.equal(report[4], '')
-    assert.equal(report[5].trim(), `${warningIcon}  2 warnings (Max Warnings: 1)`)
-    assert.equal(report[6], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should log kill message if provided', () => {
-    stylintInstance.config.maxErrors = 1
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.errs = ['', '']
-    stylintInstance.cache.warnings = []
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
-    stylintInstance.cache.lineNo = 10
-    stylintInstance.state.severity = 'Warning'
-    stylintInstance.reporter('dee')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done', 'kill').msg
+  t.ok(report.length == 7)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3].trim() == 'line 10:  dee')
+  t.ok(report[4] == '')
+  t.ok(report[5].trim() == `${warningIcon}  2 warnings (Max Warnings: 1)`)
+  t.ok(report[6] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should log kill message if provided', t => {
+  t.context.stylintInstance.config.maxErrors = 1
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.errs = ['', '']
+  t.context.stylintInstance.cache.warnings = []
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
+  t.context.stylintInstance.cache.lineNo = 10
+  t.context.stylintInstance.state.severity = 'Warning'
+  t.context.stylintInstance.reporter('dee')
 
-    assert.equal(report.length, 8)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3].trim(), 'line 10:  dee')
-    assert.equal(report[4], '')
-    assert.equal(report[5].trim(), `${errorIcon}  2 errors (Max Errors: 1)`)
-    assert.equal(report[6], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done', 'kill').msg
 
-  it('should report violations with absolute path', () => {
-    stylintInstance.config.reporterOptions = { absolutePath: true }
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.warnings = ['']
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 8)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3].trim() == 'line 10:  dee')
+  t.ok(report[4] == '')
+  t.ok(report[5].trim() == `${errorIcon}  2 errors (Max Errors: 1)`)
+  t.ok(report[6] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should report violations with absolute path', t => {
+  t.context.stylintInstance.config.reporterOptions = {absolutePath: true}
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.warnings = ['']
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
 
-    assert.equal(report.length, 6)
-    assert.equal(report[0], '')
-    assert.equal(report[1], path.join(process.cwd(), 'file.styl'))
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3], '')
-    assert.equal(report[4].trim(), `${warningIcon}  1 warning`)
-    assert.equal(report[5], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should report violations with absolute path with file being relative path', () => {
-    stylintInstance.config.reporterOptions = { absolutePath: true }
-    stylintInstance.cache.file = 'file.styl'
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.warnings = ['']
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 6)
+  t.ok(report[0] == '')
+  t.ok(report[1] == path.join(process.cwd(), 'file.styl'))
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3] == '')
+  t.ok(report[4].trim() == `${warningIcon}  1 warning`)
+  t.ok(report[5] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should report violations with absolute path with file being relative path', t => {
+  t.context.stylintInstance.config.reporterOptions = {absolutePath: true}
+  t.context.stylintInstance.cache.file = 'file.styl'
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.warnings = ['']
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
 
-    assert.equal(report.length, 6)
-    assert.equal(report[0], '')
-    assert.equal(report[1], path.join(process.cwd(), 'file.styl'))
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3], '')
-    assert.equal(report[4].trim(), `${warningIcon}  1 warning`)
-    assert.equal(report[5], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should report violations with max errors', () => {
-    stylintInstance.config.maxErrors = 0
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.errs = ['meep']
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 6)
+  t.ok(report[0] == '')
+  t.ok(report[1] == path.join(process.cwd(), 'file.styl'))
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3] == '')
+  t.ok(report[4].trim() == `${warningIcon}  1 warning`)
+  t.ok(report[5] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should report violations with max errors', t => {
+  t.context.stylintInstance.config.maxErrors = 0
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.errs = ['meep']
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
 
-    assert.equal(report.length, 6)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop')
-    assert.equal(report[3], '')
-    assert.equal(report[4].trim(), `${errorIcon}  1 error (Max Errors: 0)`)
-    assert.equal(report[5], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
 
-  it('should report original line if verbose', () => {
-    stylintInstance.config.reporterOptions = { verbose: true }
-    stylintInstance.cache.file = path.resolve('file.styl')
-    stylintInstance.cache.lineNo = 15
-    stylintInstance.cache.errs = []
-    stylintInstance.cache.warnings = ['']
-    stylintInstance.cache.origLine = 'beep boop moop'
-    stylintInstance.state.severity = ''
-    stylintInstance.reporter('woop')
+  report = chalk.stripColor(report).split('\n')
 
-    var report = stylintInstance.reporter('meh', 'done').msg
+  t.ok(report.length == 6)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop')
+  t.ok(report[3] == '')
+  t.ok(report[4].trim() == `${errorIcon}  1 error (Max Errors: 0)`)
+  t.ok(report[5] == '')
+})
 
-    report = chalk.stripColor(report).split('\n')
+test('should report original line if verbose', t => {
+  t.context.stylintInstance.config.reporterOptions = {verbose: true}
+  t.context.stylintInstance.cache.file = path.resolve('file.styl')
+  t.context.stylintInstance.cache.lineNo = 15
+  t.context.stylintInstance.cache.errs = []
+  t.context.stylintInstance.cache.warnings = ['']
+  t.context.stylintInstance.cache.origLine = 'beep boop moop'
+  t.context.stylintInstance.state.severity = ''
+  t.context.stylintInstance.reporter('woop')
 
-    assert.equal(report.length, 6)
-    assert.equal(report[0], '')
-    assert.equal(report[1], 'file.styl')
-    assert.equal(report[2].trim(), 'line 15:  woop  beep boop moop')
-    assert.equal(report[3], '')
-    assert.equal(report[4].trim(), `${warningIcon}  1 warning`)
-    assert.equal(report[5], '')
-  })
+  var report = t.context.stylintInstance.reporter('meh', 'done').msg
+
+  report = chalk.stripColor(report).split('\n')
+
+  t.ok(report.length == 6)
+  t.ok(report[0] == '')
+  t.ok(report[1] == 'file.styl')
+  t.ok(report[2].trim() == 'line 15:  woop  beep boop moop')
+  t.ok(report[3] == '')
+  t.ok(report[4].trim() == `${warningIcon}  1 warning`)
+  t.ok(report[5] == '')
 })
